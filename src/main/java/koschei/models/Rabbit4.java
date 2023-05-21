03a4885d9a70308ee281a2fdfdd5084788b3406a
f62264 <f62264@gmail.com>
2023-05-21 14:02:47 +0500

    Fixed

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck;
+    private Duck5 duck;
+
     @Autowired
-    public Rabbit4(Duck5 duck) {
+    public void setDuck(Duck5 duck) {
         this.duck = duck;
     }
+
     @Override
     public String toString() {
         return ", в зайце утка " + duck.toString();
